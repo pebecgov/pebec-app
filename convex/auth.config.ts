15c1452ffe8c1d98ce83f7c7e394cf5808d8379f
pebecgov <support@pebecgov.com>
2025-05-23 18:43:18 +0300

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 // 🚨 This project contains licensed components. Unauthorized use outside this project is prohibited and may result in legal action.
 export default {
   providers: [{
-    domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
-    applicationID: "convex"
+    domain: "https://clerk.pebec.gov.ng",
+      applicationID: "convex", 
   }]
 };
