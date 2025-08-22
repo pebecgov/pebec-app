e88eb206eef1492013cc33f48c65edf19a4ca188
ABDULLAHI BASHIR <abdullahibbtwd@gmail.com>
2025-08-22 16:23:24 +0100

    send letter layout

--- app/(site)/world_bank/received-letters/layout.tsx
+++
@@ -1,9 +0,0 @@
-import WorldBankLayoutClient from "../WorldBankLayoutClient";
-
-export default function WorldBankReceivedLettersLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <WorldBankLayoutClient>{children}</WorldBankLayoutClient>;
-}
--- app/(site)/world_bank/send-letters/layout.tsx
+++
@@ -1,9 +0,0 @@
-import WorldBankLayoutClient from "../WorldBankLayoutClient";
-
-export default function WorldBankSendLettersLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <WorldBankLayoutClient>{children}</WorldBankLayoutClient>;
-}
