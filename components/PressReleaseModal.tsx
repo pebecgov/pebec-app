ea4ee88b9ccf1d4e81886be67434a19ddd3837b3
Ajuka David <ajuka.zephiniah@gmail.com>
2025-11-26 13:53:24 +0100

    update event details

--- components/PressReleaseModal.tsx
+++ components/PressReleaseModal.tsx
@@ -131,7 +131,7 @@ export default function PressReleaseModal() {
                   className="inline-flex w-full justify-center rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:w-auto"
                   onClick={() => setIsOpen(false)}
                 >
-                  Got it, thanks!
+                  Understood
                 </button>
                 <button
                   type="button"
